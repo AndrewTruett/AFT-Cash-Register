48690a37cdc0394524f7ab6faed22756fd0aa441
Andrew Truett <andrewtruett@aol.com>
2018-03-22 20:24:48 -0400

    Added template for cash register window

--- window.py
+++ window.py
@@ -2,7 +2,17 @@ from tkinter import *
 import os
 
 class LoginWindow:
-    def __init__(self, master):
+    
+    def checkLogin(self):
+        #Check the values of the login fields in the database
+        #If login is correct, open cash register screen
+        print("ehllo")
+        pass
+    def signup(self):
+        #open a new screen that allows user to create account
+        pass
+    
+    def __init__(self, master):#Constructor
         self.master = master
         master.title('Login')
 
@@ -10,8 +20,8 @@ class LoginWindow:
         welcome = Label(master, text='Welcome to AFT Cash Register!')
         welcome.grid(columnspan=2, row=0, column=0) 
         
-        intruction = Label(master, text='Please log in') 
-        intruction.grid(row=1, column=0, sticky=E) 
+        intruction = Label(master, text='Please login') 
+        intruction.grid(row=1, column=0, sticky=W) 
  
         nameL = Label(master, text='Username: ') 
         pwordL = Label(master, text='Pin number: ') 
@@ -25,19 +35,13 @@ class LoginWindow:
         pwordE.grid(row=3, column=1)
 
         #Buttons
-        loginB = Button(master, text='Log in')
+        loginB = Button(master, text='Log in', command=self.checkLogin)
         createAccountB = Button(master, text="Create an Account!")
         loginB.grid(row=4, column=0)
         createAccountB.grid(row=4, column=1)
         
         master.mainloop()
-
-    def checkLogin():
-        #Check the values of the login fields in the database
-        pass
-    def signup():
-        #open a new screen that allows user to create account
-        pass
+        
         
 
 root = Tk() 
