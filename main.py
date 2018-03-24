c51ee554acf3f6beb1b17a37372866947af48726
AndrewTruett <andrewtruett@aol.com>
2018-03-24 18:51:21 -0400

    Incorperated window.py into main.py. Now, when the user clicks login, the main screen appears

--- main.py
+++ main.py
@@ -114,8 +114,53 @@ class CheckoutFrame(tk.Frame):
     def payCard(self):
         #Open small window which just has a button that says swipe card
         pass
+
+
+class LoginWindow(Tk):
     
+    def checkLogin(self):
+        #Check the values of the login fields in the database
+        #If login is correct, open cash register screen
 
+        self.master.destroy()
+        app = CashRegister()#for testing just open the cash register
+        app.mainloop()
+        
+        pass
+    
+    def signup(self):
+        #open a new screen that allows user to create account
+        pass
+    
+    def __init__(self, master):#Constructor
+        self.master = master
+        master.title('Login')
 
-app = CashRegister()
-app.mainloop()
+        #Labels
+        welcome = Label(master, text='Welcome to AFT Cash Register!')
+        welcome.grid(columnspan=2, row=0, column=0) 
+        
+        intruction = Label(master, text='Please login') 
+        intruction.grid(row=1, column=0, sticky=W) 
+ 
+        nameL = Label(master, text='Username: ') 
+        pwordL = Label(master, text='Pin number: ') 
+        nameL.grid(row=2, column=0, sticky=W) 
+        pwordL.grid(row=3, column=0, sticky=W) 
+
+        #Entries
+        nameE = Entry(master) 
+        pwordE = Entry(master, show='*')
+        nameE.grid(row=2, column=1) 
+        pwordE.grid(row=3, column=1)
+
+        #Buttons
+        loginB = Button(master, text='Log in', command=self.checkLogin)
+        createAccountB = Button(master, text="Create an Account!")
+        loginB.grid(row=4, column=0)
+        createAccountB.grid(row=4, column=1)
+        
+        master.mainloop()
+        
+LoginWindow(Tk())
+    
